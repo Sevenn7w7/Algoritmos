8688aa3e73e5c06840bf82fcebc4cfd7e5fba65c
Sevenn7w7 <April786660@gmail.com>
2026-09-11 21:11:13 -0600

    se agrega el proyecto

---
+++ src/main/java/main/Algoritmos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main;
+
+/**
+ *
+ * @author April
+ */
+public class Algoritmos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
